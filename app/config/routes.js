b35631050a4a4787f4b4579d8227fce6868509a5
caigua <caigua@huanleguang.com>
2019-07-09 15:47:55 +0800

    basic-shapes: triangle

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,19 +4,28 @@
 
 const NotFound = () => import('../pages/404');
 const Index = () => import('../pages/index');
+const BasicShapes = () => import('../pages/basic-shapes/index');
+const Triangle = () => import('../pages/basic-shapes/triangle');
 
 export default [
     {
         path: '/',
-        redirect: '/index'
+        redirect: '/index',
     },{
         path: '/index',
-        component: Index
+        component: Index,
+    },
+
+    {
+        path: '/basic-shapes',
+        component: BasicShapes,
+    },{
+        path: '/basic-shapes/triangle',
+        component: Triangle,
     },
 
     {
         path: '*',
-        component: NotFound
-    }
-
+        component: NotFound,
+    },
 ];
---
+++ app/pages/basic-shapes/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <categories :categories="categories"></categories>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            categories: [
+                {
+                    name: '三角形',
+                    instances: [
+                        {
+                            name: 'triangle',
+                            route: '/basic-shapes/triangle',
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+};
+</script>
